58089120ee0bd64ff7dc58c0a8aa395310c37e8f
“Gabriel <20230454ricaldone.edu.sv>
2024-02-23 12:01:38 -0600

    v0 suma

---
+++ script.js
@@ -0,0 +1,9 @@
+function Sumar(){
+    let num1 = document.getElementById("Numero1").value
+    let num2 = document.getElementById("Numero2").value
+    let sum = parseFloat(num1) + parseFloat(num2)
+    document.getElementById("valor").innerHTML = sum
+}
+
+
+
